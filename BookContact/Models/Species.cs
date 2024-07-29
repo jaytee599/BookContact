a2505c762100d4e2a365993eb618a5a2cb4a2152
eakelvin <akabakelvin@gmail.com>
2024-07-29 01:01:44 +0000

    Add project files.

---
+++ BookContact/Models/Species.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookContact.Models
+{
+    public class Species
+    {
+
+        [Key]
+        public int SpeciesID { get; set; }
+
+        public string SpeciesName { get; set; }
+
+        public bool SpeciesEndangered { get; set; }
+    }
+
+    public class SpeciesDto
+    {
+        public int SpeciesID { get; set; }
+        public string SpeciesName { get; set; }
+        public bool SpeciesEndangered { get; set; }
+
+    }
+}
---
+++ BookContact/Models/ViewModels/DetailsAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookContact.Models.ViewModels
+{
+    public class DetailsAnimal
+    {
+
+        public AnimalDto SelectedAnimal { get; set; }
+        public IEnumerable<KeeperDto> ResponsibleKeepers { get; set; }
+
+        public IEnumerable<KeeperDto> AvailableKeepers { get; set; }
+    }
+}
