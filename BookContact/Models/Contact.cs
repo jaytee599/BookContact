fb4af3b8c7fdec3d260eaebb55c6dea8c698863f
eakelvin <akabakelvin@gmail.com>
2024-07-29 18:03:22 +0000

    test clone

--- BookContact/Models/Contact.cs
+++ BookContact/Models/Contact.cs
@@ -10,10 +10,13 @@ namespace BookContact.Models
     {
         [Key]
         public int ContactId { get; set; }
-        public int UserId { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
     }
 }
